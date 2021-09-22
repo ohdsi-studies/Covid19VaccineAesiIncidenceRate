62a32e5928b672f7126512b782c55f3ea29c8cf1
Gowtham Rao <rao@ohdsi.org>
2021-09-22 08:44:24 -0400

    Updated code to run example

--- extras/exampleCodeToRun/exampleRedshift.R
+++ extras/exampleCodeToRun/exampleRedshift.R
@@ -71,22 +71,22 @@ databaseId <- connectionSpecifications$database
 getDataSourceInformation <-
   function(connectionDetails,
            cdmDatabaseSchema,
-           vocabDatabaseSchema) {
-    connection <-
-      DatabaseConnector::connect(connectionDetails = connectionDetails)
+           vocabDatabaseSchema,
+           databaseId) {
     sqlCdmDataSource <- "select * from @cdmDatabaseSchema.cdm_source;"
     sqlVocabularyVersion <-
       "select * from @vocabDatabaseSchema.vocabulary where vocabulary_id = 'None';"
+    etlVersionNumber <-
+      "select * from @cdmDatabaseSchema._version;"
+    sourceInfo <- list(cdmSourceName = databaseId,
+                       sourceDescription = databaseId)
     
-    cdmDataSource <-
-      DatabaseConnector::renderTranslateQuerySql(
-        connection = connection,
-        sql = sqlCdmDataSource,
-        cdmDatabaseSchema = cdmDatabaseSchema,
-        snakeCaseToCamelCase = TRUE
-      ) %>%
-      dplyr::tibble() %>%
-      dplyr::rename(vocabularyVersionCdm = .data$vocabularyVersion)
+    if (!is.null(connectionDetails)) {
+      connection <-
+        DatabaseConnector::connect(connectionDetails = connectionDetails)
+    } else {
+      return(NULL)
+    }
     
     vocabulary <-
       DatabaseConnector::renderTranslateQuerySql(
@@ -98,35 +98,187 @@ getDataSourceInformation <-
       dplyr::tibble() %>%
       dplyr::rename(vocabularyVersion = .data$vocabularyVersion)
     
-    DatabaseConnector::disconnect(connection = connection)
-    return((
-      if (nrow(cdmDataSource) > 0) {
-        tidyr::crossing(cdmDataSource, vocabulary) %>%
-          dplyr::mutate(
-            databaseDescription =
-              .data$sourceDescription
+    tryCatch(
+      expr = {
+        cdmDataSource <-
+          DatabaseConnector::renderTranslateQuerySql(
+            connection = connection,
+            sql = sqlCdmDataSource,
+            cdmDatabaseSchema = cdmDatabaseSchema,
+            snakeCaseToCamelCase = TRUE
           )
-      } else {
-        vocabulary
+        if (nrow(cdmDataSource) == 0) {
+          warning("cdmDataSource table has no data")
+          return(sourceInfo)
+        } else {
+          cdmDataSource <- cdmDataSource %>%
+            dplyr::tibble() %>%
+            dplyr::rename(vocabularyVersionCdm = .data$vocabularyVersion)
+        }
+        sourceInfo$sourceDescription <- ""
+        if ("sourceDescription" %in% colnames(cdmDataSource)) {
+          sourceInfo$sourceDescription <- paste0(sourceInfo$sourceDescription,
+                                                 " ",
+                                                 cdmDataSource$sourceDescription) %>%
+            stringr::str_trim()
+        }
+        if ("cdmSourceName" %in% colnames(cdmDataSource)) {
+          sourceInfo$cdmSourceName <- cdmDataSource$cdmSourceName
+        } else {
+          warning("cdmSourceName column not found in cdmDataSource table")
+        }
+        if ("cdmEtlReference" %in% colnames(cdmDataSource)) {
+          sourceInfo$sourceDescription <- paste0(
+            sourceInfo$sourceDescription,
+            " ETL Reference: ",
+            cdmDataSource$cdmEtlReference
+          ) %>%
+            stringr::str_trim()
+        } else {
+          warning("cdmEtlReference column not found in cdmDataSource table")
+          sourceInfo$sourceDescription <-
+            paste0(sourceInfo$sourceDescription,
+                   " ETL Reference: None") %>%
+            stringr::str_trim()
+        }
+        if ("cdmReleaseDate" %in% colnames(cdmDataSource)) {
+          sourceInfo$sourceDescription <- paste0(
+            sourceInfo$sourceDescription,
+            " CDM release date: ",
+            as.character(cdmDataSource$cdmReleaseDate)
+          ) %>%
+            stringr::str_trim()
+        } else {
+          warning("cdmReleaseDate column not found in cdmDataSource table")
+          sourceInfo$sourceDescription <-
+            paste0(sourceInfo$sourceDescription,
+                   " CDM release date: None") %>%
+            stringr::str_trim()
+        }
+        if ("sourceReleaseDate" %in% colnames(cdmDataSource)) {
+          sourceInfo$sourceDescription <- paste0(
+            sourceInfo$sourceDescription,
+            " Source release date: ",
+            as.character(cdmDataSource$sourceReleaseDate)
+          ) %>%
+            stringr::str_trim()
+        } else {
+          warning("sourceReleaseDate column not found in cdmDataSource table")
+          sourceInfo$sourceDescription <-
+            paste0(sourceInfo$sourceDescription,
+                   " Source release date: None") %>%
+            stringr::str_trim()
+        }
+        if ("sourceDocumentationReference" %in% colnames(cdmDataSource)) {
+          sourceInfo$sourceDescription <- paste0(
+            sourceInfo$sourceDescription,
+            " Source Documentation Reference: ",
+            as.character(cdmDataSource$sourceDocumentationReference)
+          ) %>%
+            stringr::str_trim()
+        } else {
+          warning("sourceDocumentationReference column not found in cdmDataSource table")
+          sourceInfo$sourceDescription <-
+            paste0(sourceInfo$sourceDescription,
+                   " Source Documentation Reference: None") %>%
+            stringr::str_trim()
+        }
+      },
+      error = function(...) {
+        return(sourceInfo)
       }
-    ))
+    )
+    
+    version <- dplyr::tibble()
+    tryCatch(
+      expr = {
+        version <-
+          DatabaseConnector::renderTranslateQuerySql(
+            connection = connection,
+            sql = etlVersionNumber,
+            cdmDatabaseSchema = cdmDatabaseSchema,
+            snakeCaseToCamelCase = TRUE
+          ) %>%
+          dplyr::tibble() %>%
+          dplyr::mutate(rn = dplyr::row_number()) %>%
+          dplyr::filter(.data$rn == 1) %>%
+          dplyr::select(-.data$rn)
+      },
+      error = function(...) {
+        warning("_version table not found")
+        return(NULL)
+      }
+    )
+    
+    if (nrow(version) == 1) {
+      if (all('versionId' %in% colnames(version),
+              'versionDate' %in% colnames(version))) {
+        cdmDataSource <- cdmDataSource %>%
+          dplyr::mutate(
+            cdmSourceAbbreviation = paste0(
+              .data$cdmSourceAbbreviation,
+              " (v",
+              version$versionId,
+              " ",
+              as.character(version$versionDate),
+              ")"
+            )
+          ) %>%
+          dplyr::mutate(
+            cdmSourceName = paste0(
+              .data$cdmSourceName,
+              " (v",
+              version$versionId,
+              " ",
+              as.character(version$versionDate),
+              ")"
+            )
+          ) %>%
+          dplyr::mutate(
+            sourceDescription = paste0(
+              .data$sourceDescription,
+              " (v",
+              version$versionId,
+              " ",
+              as.character(version$versionDate),
+              ")"
+            )
+          )
+      }
+    }
+    
+    DatabaseConnector::disconnect(connection = connection)
+    return(if (nrow(cdmDataSource) > 0) {
+      tidyr::crossing(cdmDataSource, vocabulary, version) %>%
+        dplyr::mutate(databaseDescription =
+                        .data$sourceDescription)
+    } else {
+      warning("cdmDataSource table has no data")
+      vocabulary
+    })
   }
 
 dataSourceInformation <- getDataSourceInformation(connectionDetails = connectionDetails,
                                                   cdmDatabaseSchema = cdmDatabaseSchema,
-                                                  vocabDatabaseSchema = connectionSpecifications$vocabDatabaseSchema)
+                                                  vocabDatabaseSchema = connectionSpecifications$vocabDatabaseSchema, 
+                                                  databaseId = databaseId)
 
+if (all(!is.null(dataSourceInformation),
+        nrow(dataSourceInformation) == 1,
+        nchar(dataSourceInformation$versionId) > 0)) {
+  databaseId <- paste0(databaseId, "_v",dataSourceInformation$versionId, "_", as.character(dataSourceInformation$versionDate))
+}
 
 databaseName <- dataSourceInformation$cdmSourceName
 databaseDescription <- dataSourceInformation$sourceDescription
 
 Covid19VaccineAesiIncidenceRate::runStudy(connectionDetails = connectionDetails,
-                                                      cdmDatabaseSchema = cdmDatabaseSchema,
-                                                      cohortDatabaseSchema = cohortDatabaseSchema,
-                                                      cohortTablePrefix = "ir",
-                                                      exportFolder = file.path(outputFolder, databaseId),
-                                                      databaseId = databaseId,
-                                                      databaseName = databaseName,
-                                                      databaseDescription = databaseDescription,
-                                                      incremental = TRUE,
-                                                      minCellCount = minCellCount)
+                                          cdmDatabaseSchema = cdmDatabaseSchema,
+                                          cohortDatabaseSchema = cohortDatabaseSchema,
+                                          cohortTablePrefix = "ir",
+                                          exportFolder = file.path(outputFolder, databaseId),
+                                          databaseId = databaseId,
+                                          databaseName = databaseName,
+                                          databaseDescription = databaseDescription,
+                                          incremental = TRUE,
+                                          minCellCount = minCellCount)
